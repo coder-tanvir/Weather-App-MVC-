ff8694b322fdd0608f0c943c79a74600be369143
coder-tanvir <tanvirrahmankhan@gmail.com>
2022-01-01 14:29:59 -0800

    Getting data from weather API

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,22 @@
+const axios = require("axios");
+const API_KEY = "c8d6a3253f4dd87064d3ee885ce99f71";
+
 exports.renderHomePage = (req, res) => {
   res.render("index");
+};
+
+exports.getWeather = (req, res) => {
+  const cityname = req.body.city;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${API_KEY}&units=metric`;
+
+  axios
+    .get(url)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.renderAboutPage = (req, res) => {
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("./controllers/controller.js");
 
 router.get("/", controller.renderHomePage);
+router.post("/", controller.getWeather);
 
 router.get("/about", controller.renderAboutPage);
 
